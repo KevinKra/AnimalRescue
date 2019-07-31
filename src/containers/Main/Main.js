1fab54885148b6b8ae38657df2e16bc0e14c3a63
Kevin Krato <kratok08@gmail.com>
2019-07-31 10:45:21 -0600

    Output Donations data

--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -15,7 +15,7 @@ export class Main extends Component {
   }
 
   render() {
-    const output = this.props.animals ? (
+    const cardsOutput = this.props.animals ? (
       this.props.animals.map(animal => (
         <Card data={{ ...animal }} key={animal.id} />
       ))
@@ -23,19 +23,26 @@ export class Main extends Component {
       <h2>Loading!</h2>
     );
 
+    const donationsOutput = this.props.donations ? (
+      this.props.donations.map(donation => (
+        <Donations data={{ ...donation }} key={donation.id} />
+      ))
+    ) : (
+      <h2>Loading!</h2>
+    );
+
     return (
       <main className="Main">
-        <section className="card-output">{output}</section>
-        <aside className="donations-output">
-          <Donations />
-        </aside>
+        <section className="card-output">{cardsOutput}</section>
+        <aside className="donations-output">{donationsOutput}</aside>
       </main>
     );
   }
 }
 
 const mapStateToProps = store => ({
-  animals: store.animals
+  animals: store.animals,
+  donations: store.donations
 });
 
 const mapDispatchToProps = dispatch => ({
