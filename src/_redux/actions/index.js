36181ec4f39ea94a3ba60f18eb643fcb190a0a1c
Kevin Krato <kratok08@gmail.com>
2019-07-31 10:39:44 -0600

    Update redux data

--- src/_redux/actions/index.js
+++ src/_redux/actions/index.js
@@ -2,3 +2,8 @@ export const collectAnimals = animals => ({
   type: "COLLECT_ANIMALS",
   animals
 });
+
+export const collectDonations = donations => ({
+  type: "COLLECT_DONATIONS",
+  donations
+});
--- src/_redux/reducers/index.js
+++ src/_redux/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from "redux";
 import { animalsReducer } from "./animalsReducer";
+import { donationsReducer } from "./donationsReducer";
 
 export const rootReducer = combineReducers({
-  animals: animalsReducer
+  animals: animalsReducer,
+  donations: donationsReducer
 });
