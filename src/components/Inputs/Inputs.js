6e58ea482ba451c37cc43a72c78b15fa78f0ceb3
Kevin Krato <kratok08@gmail.com>
2019-07-31 11:08:14 -0600

    Update redux and store on button submit

--- src/components/Inputs/Inputs.js
+++ src/components/Inputs/Inputs.js
@@ -1,6 +1,8 @@
 import React, { Component } from "react";
+import { connect } from "react-redux";
+import * as actions from "../../_redux/actions";
 
-export default class Inputs extends Component {
+class Inputs extends Component {
   state = {
     donorName: "",
     donation: ""
@@ -13,18 +15,51 @@ export default class Inputs extends Component {
 
   handleSubmit = e => {
     e.preventDefault();
-    console.log("submitted");
+    this.postDonor();
+  };
+
+  postDonor = async () => {
+    const donorInfo = {
+      id: Date.now(),
+      name: this.state.donorName,
+      donation: this.state.donation
+    };
+    const response = await fetch("http://localhost:3001/api/v1/donations/", {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify(donorInfo)
+    });
+    this.props.updateDonations(await response.json());
   };
 
   render() {
     return (
       <form>
         <label htmlFor="animal-name" />
-        <input type="text" name="donorName" onChange={this.collectInputs} />
+        <input
+          type="text"
+          name="donorName"
+          value={this.state.donorName}
+          onChange={this.collectInputs}
+        />
         <label htmlFor="donation" />
-        <input type="text" name="donation" onChange={this.collectInputs} />
+        <input
+          type="text"
+          name="donation"
+          value={this.state.donation}
+          onChange={this.collectInputs}
+        />
         <button onClick={this.handleSubmit}>Donate!</button>
       </form>
     );
   }
 }
+
+const mapDispatchToProps = dispatch => ({
+  updateDonations: donation => dispatch(actions.updateDonations(donation))
+});
+
+export default connect(
+  null,
+  mapDispatchToProps
+)(Inputs);
