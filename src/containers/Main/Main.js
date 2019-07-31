225852912a9e2b230a7e4290ce59e2182406b9c5
Kevin Krato <kratok08@gmail.com>
2019-07-31 11:56:28 -0600

    Convert to functional component and extract dispatching to app

--- src/containers/Main/Main.js
+++ src/containers/Main/Main.js
@@ -3,41 +3,30 @@ import Card from "../../components/Card/Card";
 import Donations from "../../components/Donations/Donations";
 import { connect } from "react-redux";
 import * as actions from "../../_redux/actions/";
-import * as apiCalls from "../../_api/";
+// import * as apiCalls from "../../_api/";
 import "./Main.css";
 
-export class Main extends Component {
-  state = {};
+function Main(props) {
+  const cardsOutput = props.animals ? (
+    props.animals.map(animal => <Card data={{ ...animal }} key={animal.id} />)
+  ) : (
+    <h2>Loading!</h2>
+  );
 
-  async componentDidMount() {
-    this.props.collectAnimals(await apiCalls.fetchData("rescue-animals"));
-    this.props.collectDonations(await apiCalls.fetchData("donations"));
-  }
+  const donationsOutput = props.donations ? (
+    props.donations.map(donation => (
+      <Donations data={{ ...donation }} key={donation.id} />
+    ))
+  ) : (
+    <h2>Loading!</h2>
+  );
 
-  render() {
-    const cardsOutput = this.props.animals ? (
-      this.props.animals.map(animal => (
-        <Card data={{ ...animal }} key={animal.id} />
-      ))
-    ) : (
-      <h2>Loading!</h2>
-    );
-
-    const donationsOutput = this.props.donations ? (
-      this.props.donations.map(donation => (
-        <Donations data={{ ...donation }} key={donation.id} />
-      ))
-    ) : (
-      <h2>Loading!</h2>
-    );
-
-    return (
-      <main className="Main">
-        <section className="card-output">{cardsOutput}</section>
-        <aside className="donations-output">{donationsOutput}</aside>
-      </main>
-    );
-  }
+  return (
+    <main className="Main">
+      <section className="card-output">{cardsOutput}</section>
+      <aside className="donations-output">{donationsOutput}</aside>
+    </main>
+  );
 }
 
 const mapStateToProps = store => ({
@@ -45,12 +34,4 @@ const mapStateToProps = store => ({
   donations: store.donations
 });
 
-const mapDispatchToProps = dispatch => ({
-  collectAnimals: animals => dispatch(actions.collectAnimals(animals)),
-  collectDonations: donations => dispatch(actions.collectDonations(donations))
-});
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Main);
+export default connect(mapStateToProps)(Main);
